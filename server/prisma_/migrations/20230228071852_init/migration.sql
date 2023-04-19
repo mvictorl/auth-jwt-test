a02d8edee626624a6e4142bc9777f3454390f74e
MVictorL <mvictorl@gmail.com>
2023-04-19 17:22:52 +0300

    Delete server/prisma_ directory

--- server/prisma_/migrations/20230228071852_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "tester"."Exercise" ADD COLUMN     "userId" TEXT NOT NULL DEFAULT '131e1ed4-e2c3-4f74-91b2-d93dd652ec9c';
-
--- AddForeignKey
-ALTER TABLE "tester"."Exercise" ADD CONSTRAINT "Exercise_userId_fkey" FOREIGN KEY ("userId") REFERENCES "auth"."User"("id") ON DELETE SET DEFAULT ON UPDATE CASCADE;
--- server/prisma_/migrations/20230303064157_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "tester"."Exercise" ADD COLUMN     "description" TEXT;
